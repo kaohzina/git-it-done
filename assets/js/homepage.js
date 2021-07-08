cf53b033fc43edd0ec3b4244341a3d786f2b49aa
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-07-08 16:29:57 -0500

    linked is single and homepage js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,12 +10,11 @@ var formSubmitHandler = function(event) {
   // check if the variable username has a value inside it and then clear it right after 
   if (username) {
     getUserRepos(username);
+    repoContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter a Github username");
   }
-  
-  console.log(event);
 };
 
 var getUserRepos = function(user) {
@@ -23,14 +22,18 @@ var getUserRepos = function(user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
-  fetch(apiUrl).then(function(response) {
-    if (response.ok){response.json().then(function(data) {
-      displayRepos(data, user);
-    });
+  fetch(apiUrl)
+    .then(function(response) {
+    if (response.ok){
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+        displayRepos(data, user);
+      });
     } else {
-        alert("Error: GitHub User Not Found");
+        alert("Error: " + response.statusText);
       }
-  })
+    })
   .catch(function(error){
     alert("Unable to connect to GitHub");
   });
@@ -42,10 +45,7 @@ var displayRepos = function(repos, searchTerm) {
     repoContainerEl.textContent = "No repositories found.";
     return;
   }
-  console.log(repos);
-  console.log(searchTerm);
-  // clear old content
-  repoContainerEl.textContent = "";
+ 
   repoSearchTerm.textContent = searchTerm;
 
   // loop over repos
@@ -54,9 +54,9 @@ var displayRepos = function(repos, searchTerm) {
   var repoName = repos[i].owner.login + "/" + repos[i].name;
 
   // create a container for each repo
-  var repoEl = document.createElement("div");
+  var repoEl = document.createElement("a");
   repoEl.classList = "list-item flex-row justify-space-between align-center";
-
+  repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
   // create a span element to hold repository name
   var titleEl = document.createElement("span");
   titleEl.textContent = repoName;
